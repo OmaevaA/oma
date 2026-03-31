6e214a31f34971b0d4e3b7847bdff2a2cb54eca1
OmaevaA <nastya-soboleva-14@mail.ru>
2026-03-31 16:23:34 +0300

    Update MainWindow.xaml.cs

--- TZOmPavMalPech/MainWindow.xaml.cs
+++ TZOmPavMalPech/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +23,32 @@ namespace TZOmPavMalPech
         public MainWindow()
         {
             InitializeComponent();
+            FrameMain.Navigate(new Pages.Autorization());
+            // инициализируем обработку события ContentRendered сразу после загрузки страницы:
+            FrameMain.ContentRendered += new EventHandler(MainFrame_ContentRendered);
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (FrameMain.CanGoBack)
+                FrameMain.GoBack();
+        }
+
+        // Скрываем кнопку назад на первой странице (странице авторизации),
+        // показываем её на последующих страницах.
+
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+
+            if (!FrameMain.CanGoBack || this.FrameMain.Content.GetType() == typeof(Pages.Autorization))
+            {
+                BtnBack.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                BtnBack.Visibility = Visibility.Visible;
+            }
         }
     }
 }
